2c3c6e655a5f696f554c109bc723129881d1d609
feng-pro <jun1st.feng@gmail.com>
2014-03-30 19:14:58 +0800

    add

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,11 +1,12 @@
 APP_PATH = File.expand_path(File.expand_path(File.dirname(__FILE__)) + "/../")
+SHARED_PATH = File.expand_path(APP_PATH + "/../")
 
 worker_processes 2
 working_directory APP_PATH
 
-listen "/tmp/campo.unicorn.sock", :backlog => 64
+listen "#{SHARED_PATH}/shared/sockets/campo.unicorn.sock", :backlog => 64
 timeout 30
 
-pid "#{APP_PATH}/tmp/pids/unicorn.pid"
+pid "#{SHARED_PATH}/shared/sockets/pids/unicorn.pid"
 stderr_path "#{APP_PATH}/log/unicorn.stderr.log"
 stdout_path "#{APP_PATH}/log/unicorn.stdout.log"
